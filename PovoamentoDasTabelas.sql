8944074adf226d6644443e1f3f4dc133c3ef7e8b
erlanl <erlanlirajr@gmail.com>
2023-03-01 19:16:47 -0300

    feat: povoa a tabela telefone

--- PovoamentoDasTabelas.sql
+++ PovoamentoDasTabelas.sql
@@ -43,3 +43,38 @@ INSERT INTO Pessoa (cpf, nome, data_nascimento, sexo) VALUES ('101.101.101.01', 
 INSERT INTO Pessoa (cpf, nome, data_nascimento, sexo) VALUES ('202.202.202-02', 'Ana Nóbrega', to_date('25/12/1984', 'dd/mm/yy'), 'F');
 INSERT INTO Pessoa (cpf, nome, data_nascimento, sexo) VALUES ('303.303.303-03', 'Marcos Antônio', to_date('07/02/1988', 'dd/mm/yy'), 'M');
 
+--Povoando Telefone
+INSERT INTO Telefone (cpf_pessoa, numero_telefone) VALUES ('999.999.999-99', '+55 81 99902 2762');
+INSERT INTO Telefone (cpf_pessoa, numero_telefone) VALUES ('999.999.999-99', '+55 81 98417 1256');
+INSERT INTO Telefone (cpf_pessoa, numero_telefone) VALUES ('999.999.999-99', '+55 81 98284 5843');
+INSERT INTO Telefone (cpf_pessoa, numero_telefone) VALUES ('888.888.888-88', '+55 27 98431 0816');
+INSERT INTO Telefone (cpf_pessoa, numero_telefone) VALUES ('888.888.888-88', '+55 27 98741 2487');
+INSERT INTO Telefone (cpf_pessoa, numero_telefone) VALUES ('777.777.777-77', '+55 82 99768 8281');
+INSERT INTO Telefone (cpf_pessoa, numero_telefone) VALUES ('666.666.666-66', '+55 69 96765 2831');
+INSERT INTO Telefone (cpf_pessoa, numero_telefone) VALUES ('666.666.666-66', '+55 69 99991 2587');
+INSERT INTO Telefone (cpf_pessoa, numero_telefone) VALUES ('555.555.555-55', '+55 98 99680 1715');
+INSERT INTO Telefone (cpf_pessoa, numero_telefone) VALUES ('555.555.555-55', '+55 98 99787 1785');
+INSERT INTO Telefone (cpf_pessoa, numero_telefone) VALUES ('555.555.555-55', '+55 98 99177 9686');
+INSERT INTO Telefone (cpf_pessoa, numero_telefone) VALUES ('555.555.555-55', '+55 98 97994 2094');
+INSERT INTO Telefone (cpf_pessoa, numero_telefone) VALUES ('444.444.444-44', '+55 49 98526 2678');
+INSERT INTO Telefone (cpf_pessoa, numero_telefone) VALUES ('444.444.444-44', '+1 408 987 4345');
+INSERT INTO Telefone (cpf_pessoa, numero_telefone) VALUES ('333.333.333-33', '+55 91 99547 0563');
+INSERT INTO Telefone (cpf_pessoa, numero_telefone) VALUES ('333.333.333-33', '+54 9 432 929 1497');
+INSERT INTO Telefone (cpf_pessoa, numero_telefone) VALUES ('333.333.333-33', '+54 9 532 999 9915');
+INSERT INTO Telefone (cpf_pessoa, numero_telefone) VALUES ('222.222.222-22', '+55 67 96917 4131');
+INSERT INTO Telefone (cpf_pessoa, numero_telefone) VALUES ('111.111.111-11', '+55 49 98234 8053');
+INSERT INTO Telefone (cpf_pessoa, numero_telefone) VALUES ('100.100.100-10', '+55 85 98570 7654');
+INSERT INTO Telefone (cpf_pessoa, numero_telefone) VALUES ('200.200.200-20', '+55 12 97288 8966');
+INSERT INTO Telefone (cpf_pessoa, numero_telefone) VALUES ('200.200.200-20', '+1 678 799 3007');
+INSERT INTO Telefone (cpf_pessoa, numero_telefone) VALUES ('300.300.300-30', '+55 91 96869 7810');
+INSERT INTO Telefone (cpf_pessoa, numero_telefone) VALUES ('400.400.400-40', '+55 71 98379 5684');
+INSERT INTO Telefone (cpf_pessoa, numero_telefone) VALUES ('500.500.500-50', '+55 22 99184 4150');
+INSERT INTO Telefone (cpf_pessoa, numero_telefone) VALUES ('600.600.600-60', '+55 84 99747 6532');
+INSERT INTO Telefone (cpf_pessoa, numero_telefone) VALUES ('600.600.600-60', '+1 408 788 3477');
+INSERT INTO Telefone (cpf_pessoa, numero_telefone) VALUES ('700.700.700-70', '+55 92 99957 0516');
+INSERT INTO Telefone (cpf_pessoa, numero_telefone) VALUES ('800.800.800-80', '+55 79 98230 6697');
+INSERT INTO Telefone (cpf_pessoa, numero_telefone) VALUES ('900.900.900-90', '+55 86 96947 4295');
+INSERT INTO Telefone (cpf_pessoa, numero_telefone) VALUES ('101.101.101.01', '+55 31 98050 6343');
+INSERT INTO Telefone (cpf_pessoa, numero_telefone) VALUES ('101.101.101.01', '+55 31 99631 3695');
+INSERT INTO Telefone (cpf_pessoa, numero_telefone) VALUES ('202.202.202-02', '+55 98 99284 3640');
+INSERT INTO Telefone (cpf_pessoa, numero_telefone) VALUES ('303.303.303-03', '+55 85 97250 2315');
