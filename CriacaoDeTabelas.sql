0e243a19eed3b6120386d7d36483aec8c650f482
GuilhermeMaciel75 <guilhermemacielmelo75@hotmail.com>
2023-03-01 22:10:59 -0300

    refactor: refatora algumas tabelas

--- CriacaoDeTabelas.sql
+++ CriacaoDeTabelas.sql
@@ -42,19 +42,19 @@ CREATE TABLE Pessoa (
 	cpf VARCHAR2(14) NOT NULL,
 	nome VARCHAR2(25) NOT NULL,
 	data_nascimento DATE NOT NULL,
-	sexo CHAR(1) NOT NULL,
+	sexo CHAR(1) NOT NULL, -- M ou F
 	CONSTRAINT pessoa_pkey PRIMARY KEY (cpf)
 );
 
 -- ENDEREÇO
 CREATE TABLE Endereco (
 	cpf_pessoa VARCHAR2(14) NOT NULL,
-	cep VARCHAR2(9) NOT NULL,
+	cep VARCHAR2(9) NOT NULL, 
 	numero NUMBER NOT NULL,
 	rua VARCHAR2(30) NOT NULL,
 	bairro VARCHAR2(25) NOT NULL,
 	cidade VARCHAR2(25) NOT NULL,
-	estado CHAR(2),
+	estado CHAR(2) NOT NULL,
 	CONSTRAINT endereco_pkey PRIMARY KEY (cpf_pessoa),
 	CONSTRAINT endereco_fkey FOREIGN KEY (cpf_pessoa) REFERENCES Pessoa (cpf)
 );
@@ -70,7 +70,7 @@ CREATE TABLE Telefone (
 -- CLIENTE
 CREATE TABLE Cliente (
   cpf_cliente VARCHAR2(14) NOT NULL,
-  metodo_pagamento VARCHAR2(20) NOT NULL,
+  metodo_pagamento VARCHAR2(20) NOT NULL, -- Cartao de Crédito, Carto de Débito, PIX, Dinheiro
   CONSTRAINT cliente_pkey PRIMARY KEY (cpf_cliente),
   CONSTRAINT cliente_fkey FOREIGN KEY (cpf_cliente) REFERENCES Pessoa (cpf)
 );
@@ -88,29 +88,12 @@ CREATE TABLE Dependente (
 -- FUNCIONÁRIO
 CREATE TABLE Funcionario (
 	cpf_funcionario VARCHAR2(14) NOT NULL,
-	salario NUMBER(5,2) NOT NULL,
+	salario NUMBER(7,2) NOT NULL,
 	cpf_supervisor VARCHAR2(14),
 	CONSTRAINT funcionario_pkey PRIMARY KEY (cpf_funcionario),
 	CONSTRAINT funcionario_fkey1 FOREIGN KEY (cpf_funcionario) REFERENCES Pessoa (cpf),
 	CONSTRAINT funcionario_fkey2 FOREIGN KEY (cpf_supervisor) REFERENCES Funcionario (cpf_funcionario),
 	CONSTRAINT funcionario_check CHECK (salario >= 1310.00) -- CLÁUSULA CHECK
-);
-
--- OPERADOR
-CREATE TABLE Operador (
-	cpf_operador VARCHAR2(14) NOT NULL,
-	brinquedo VARCHAR2(25) NOT NULL,
-	CONSTRAINT operador_pkey PRIMARY KEY (cpf_operador),
-	CONSTRAINT operador_fkey1 FOREIGN KEY (cpf_operador) REFERENCES Funcionario(cpf_funcionario),
-	CONSTRAINT operador_fkey2 FOREIGN KEY (brinquedo) REFERENCES Brinquedo(nome)
-);
-
--- ATENDENTE
-CREATE TABLE Atendente (
-	cpf_atendente VARCHAR2(14) NOT NULL,
-	banca VARCHAR2(25) NOT NULL,
-	CONSTRAINT atendente_pkey PRIMARY KEY (cpf_atendente),
-	CONSTRAINT atendente_fkey FOREIGN KEY (cpf_atendente) REFERENCES Funcionario(cpf_funcionario)
 );
 
 -- AREA
@@ -127,10 +110,27 @@ CREATE TABLE Brinquedo (
     area VARCHAR2(25) NOT NULL,
     capacidade NUMBER(2) NOT NULL,
     restricao_de_idade NUMBER(2) NOT NULL,
-    restricao_de_altura NUMBER(3,2) NOT NULL,
+    restricao_de_altura NUMBER(3,2) NOT NULL, --Altura em metros
     
     CONSTRAINT brinquedo_pkey PRIMARY KEY (nome),
     CONSTRAINT area_fkey FOREIGN KEY (area) REFERENCES Area (categoria)
+);
+
+-- ATENDENTE
+CREATE TABLE Atendente (
+	cpf_atendente VARCHAR2(14) NOT NULL,
+	banca VARCHAR2(25) NOT NULL,
+	CONSTRAINT atendente_pkey PRIMARY KEY (cpf_atendente),
+	CONSTRAINT atendente_fkey FOREIGN KEY (cpf_atendente) REFERENCES Funcionario(cpf_funcionario)
+);
+
+-- OPERADOR
+CREATE TABLE Operador (
+	cpf_operador VARCHAR2(14) NOT NULL,
+	brinquedo VARCHAR2(25) NOT NULL,
+	CONSTRAINT operador_pkey PRIMARY KEY (cpf_operador),
+	CONSTRAINT operador_fkey1 FOREIGN KEY (cpf_operador) REFERENCES Funcionario(cpf_funcionario),
+	CONSTRAINT operador_fkey2 FOREIGN KEY (brinquedo) REFERENCES Brinquedo(nome)
 );
 
 -- JOGA
@@ -153,17 +153,19 @@ CREATE TABLE Brinca (
 	CONSTRAINT brinca_fkey2 FOREIGN KEY (cliente) REFERENCES Cliente (cpf_cliente)
 );
 
+CREATE SEQUENCE codigo_sequencia INCREMENT BY 1 START WITH 1;
+
 -- INGRESSO
 CREATE TABLE Ingresso (
-	codigo NUMBER(3) NOT NULL,
-	valor NUMBER(3,2) NOT NULL,
+	codigo_sequencia INTEGER NOT NULL,
+	valor NUMBER(5,2) NOT NULL,
 	CONSTRAINT ingresso_pkey PRIMARY KEY (codigo),
 );
 
 -- PROMOÇÃO
 CREATE TABLE Promocao (
-	codigo_promocao NUMBER(3) NOT NULL,
-	desconto NUMBER(2) NOT NULL,
+	codigo_promocao NUMBER(3) NOT NULL, --Tipo de promocao 
+	desconto NUMBER(2) NOT NULL, --Em porcentual
 	restricao char(1) NOT NULL, -- S (Sim) / N(Não)??
 	data_inicio DATE NOT NULL,
 	data_termino DATE NOT NULL,
@@ -174,7 +176,7 @@ CREATE TABLE Promocao (
 
 -- BILHETERIA
 CREATE TABLE Bilheteria (
-	ingresso NUMBER(3) NOT NULL,
+	ingresso INTEGER NOT NULL,
 	cliente VARCHAR2(14) NOT NULL,
 	atendete VARCHAR2(14) NOT NULL,
 	promocao NUMBER(3) NOT NULL,
@@ -182,8 +184,8 @@ CREATE TABLE Bilheteria (
 
 
 	CONSTRAINT bilheteria_pkey PRIMARY KEY (ingresso, data_e_hora),
-	CONSTRAINT bilheteria_fkey1 FOREIGN KEY (ingresso) REFERENCES Ingresso (codigo)
+	CONSTRAINT bilheteria_fkey1 FOREIGN KEY (ingresso) REFERENCES Ingresso (codigo_sequencia),
 	CONSTRAINT bilheteria_fkey2 FOREIGN KEY (cliente) REFERENCES Cliente (cpf_cliente),
 	CONSTRAINT bilheteria_fkey3 FOREIGN KEY (atendete) REFERENCES Atendente (cpf_atendente),
-	CONSTRAINT bilheteria_fkey4 FOREIGN KEY (promocao) REFERENCES Promocao (codigo)
+	CONSTRAINT bilheteria_fkey4 FOREIGN KEY (promocao) REFERENCES Promocao (codigo_promocao)
 );
