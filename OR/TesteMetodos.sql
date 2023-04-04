efec1f2bae48758d34e7a79747952f7d49876578
erlanl <erlanlirajr@gmail.com>
2023-04-04 12:41:39 -0300

    feat: adiciona teste para exibir informacoes de uma pessoa

--- OR/TesteMetodos.sql
+++ OR/TesteMetodos.sql
@@ -1,6 +1,26 @@
 --Teste CONSTRUCTOR FUNCTION tp_endereco
 
---Teste PROCEDURE exibirInformacoes OVERRIDING
+--Teste PROCEDURE exibirInformacoes
+DECLARE
+    cliente tp_cliente;
+BEGIN
+    SELECT VALUE(P) INTO cliente FROM tb_cliente P
+    WHERE P.cpf = '111.111.111-11';
+
+    cliente.exibirInformacoes();
+END;
+/
+
+--OVERRIDING
+DECLARE
+    operador tp_operador;
+BEGIN
+    SELECT VALUE(P) INTO operador FROM tb_operador P
+    WHERE P.cpf = '808.808.808-08';
+
+    operador.exibirInformacoes();
+END;
+/
 
 --Teste MEMBER FUNCTION salarioBonusNatal
 
