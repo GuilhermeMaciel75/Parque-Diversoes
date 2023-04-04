3a9b05244c323992f050b97aa42f1fbdc0582499
Amanda-Quirino <aqrs@cin.ufpe.br>
2023-04-04 12:47:19 -0300

    test: Teste do MAP MEMBER adicionado

--- OR/TesteMetodos.sql
+++ OR/TesteMetodos.sql
@@ -1,9 +1,24 @@
 --Teste CONSTRUCTOR FUNCTION tp_endereco
 
---Teste PROCEDURE exibirInformacoes OVERRIDING
-
---Teste MEMBER FUNCTION salarioBonusNatal
+--Teste PROCEDURE exibirInformacoes OVERRIDING e de MEMBER FUNCTION salarioBonusNatal
+DECLARE
+    V_atendente tp_atendente;
+	V_SALARIO NUMBER;
+BEGIN
+	SELECT VALUE(A) INTO V_atendente FROM tb_atendente A WHERE A.cpf = '300.300.300-30';
+	V_atendente.exibirInformacoes();
+    V_SALARIO := V_atendente.salarioBonusNatal();
+	DBMS_OUTPUT.PUT_LINE('Salario com Bonus: ' || V_SALARIO);
+END;
 
 --Teste FINAL ORDER MEMBER FUNCTION comparaQtd
 
 --Teste MAP MEMBER FUNCTION descontoPorc
+DECLARE
+    porcentagem NUMBER;
+	promocao tp_promocao;
+BEGIN
+    SELECT VALUE(P) INTO promocao FROM tb_promocao P WHERE P.codigo_promocao = 1;
+	porcentagem := promocao.descontoPorc();
+	DBMS_OUTPUT.PUT_LINE('Porcentagem paga: ' || porcentagem);
+END;
