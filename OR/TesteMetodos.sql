571b7b691c0f2ba2d9b4dcc610bf9b13bf496dae
GuilhermeMaciel75 <guilhermemacielmelo75@hotmail.com>
2023-04-04 11:58:52 -0300

    add arquivo de testes

---
+++ OR/TesteMetodos.sql
@@ -0,0 +1,9 @@
+--Teste CONSTRUCTOR FUNCTION tp_endereco
+
+--Teste PROCEDURE exibirInformacoes OVERRIDING
+
+--Teste MEMBER FUNCTION salarioBonusNatal
+
+--Teste FINAL ORDER MEMBER FUNCTION comparaQtd
+
+--Teste MAP MEMBER FUNCTION descontoPorc
